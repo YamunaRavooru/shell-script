512d38bd8af52bd077df7d68c692874f9d718439
yamuna <malini.mahathi@gmail.com>
2024-12-31 17:11:49 +0530

    scripting for special var

--- 08-if-condition.sh
+++ 08-if-condition.sh
@@ -2,7 +2,7 @@
 TODAY=$1
 DAYS=("mon" "Tue""wed""Thu""Fri""Sat""Sunday")
 echo "enter today is::$TODAY"
-if ( $TODAY -eq ${DAYS[6]})
+if (( $TODAY -eq ${DAYS[6]}))
 then
   echo "today is $TODAY :: holiday"
  else
