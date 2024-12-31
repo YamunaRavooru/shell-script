c6c0d7b1a7101a83c19df9d51f60706058b15c79
yamuna <malini.mahathi@gmail.com>
2024-12-31 15:58:56 +0530

    scripting for sum of 2 num

--- 05-datatype.sh
+++ 05-datatype.sh
@@ -5,6 +5,6 @@ NUMBER2=$2
 TIMESTAMP=$(date)
 echo "script executed at:$TIMESTAMP"
 echo "number1=$NUMBER1 number2=$NUMBER2"
-SUM=$(($Number1+$Number2))
+SUM=$(($NUMBER1+$NUMBER2))
 
 echo "sum of 2 numbers is:$SUM"
