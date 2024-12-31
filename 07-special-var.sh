e2bab3f979711bdbe2506f93d845f0c2ba89b84f
yamuna <malini.mahathi@gmail.com>
2024-12-31 16:41:38 +0530

    scripting for special var

---
+++ 07-special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables:: $@"
+echo "Number of variables:: $#"
+echo "Script Name:: $0"
+echo "Present working directory::$PWS"
+echo "Home  Directory of current user::$HOME"
+echo "Which user running the script::$USER"
+echo "Process id of current script::$$"
+sleep=100 &
+echo "process id of last command in background::$!"
