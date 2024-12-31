90f2d9d09a7a897de664b31054248cd6273cc0b0
yamuna <malini.mahathi@gmail.com>
2024-12-31 16:16:05 +0530

    scripting for array list

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "rrr" "devara")
+echo "First movie=${MOVIES[0]}"
+echo "Second movie=${MOVIES[1]}"
+echo "third movie=${MOVIEs[2]}"
+
+echo "all the movies are::${movies[@]}"
