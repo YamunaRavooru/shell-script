ac97b0083bf217ddb432e9e58b50a6d4d3a5c863
yamuna <malini.mahathi@gmail.com>
2024-12-31 15:47:34 +0530

    i am adding script for sum of 2 num

--- 05-datatype.sh
+++ 05-datatype.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Number=$1
-Number=$2
-Timestamp=$(date)
-echo "script executed at:$Timestamp"
-Sum=$(($Number1+$Number2))
-echo "sum of 2 numbers is:$sum"
+NUMBER1=$1
+NUMBER2=$2
+TIMESTAMP=$(date)
+echo "script executed at:$TIMESTAMP"
+SUM=$(($Number1+$Number2))
+echo "sum of 2 numbers is:$SUM"
