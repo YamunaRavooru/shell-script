558aaf1d577e3e487d043f557506374753a188ff
yamuna <malini.mahathi@gmail.com>
2025-01-02 13:48:29 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [ $USERID -eq 0 ] 
-then
-    dnf list intalled mysql
-    if [ $? -ne 0 ]
-     then
-      dnf install mysql -y
-    #   if [ $? -eq 0 ]
-    #   then 
-    #   echo "mysql installed.....sucessfully"
-    #  else
-    #   echo "mysql installed....failure"
-    #  fi
-     else
-      echo "mysql already.....installed" 
-    fi 
-    #    dnf install git -y
+if [ $USERID -ne 0 ] 
+then 
+echo "Error: you must have root acess fot this script"
+fi
 
-    # if [ $? -eq 0 ]
-    #  then
-    #     echo "MYSQL Installed....Successful"
-    #     echo "Git Installed.... Sucessful"
-    #    else 
-    #     echo "MYSQL Installed....Failure"
-    #     echo "Git Installed.... Failure"
-    #  fi   
-  else      
-        echo "Error: you must have sudo access to run this script"
-  fi 
-   
-    
-  
+dnf install mysql -y
+dnf install git -y
 
 
