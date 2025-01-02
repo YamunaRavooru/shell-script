221ee216ecbbd2d0c7541c563b7e74d561c6f5fe
yamuna <malini.mahathi@gmail.com>
2025-01-02 13:28:14 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -4,20 +4,11 @@ USERID=$(id -u)
 if [ $USERID -eq 0 ] 
 then
     dnf list intalled mysql
-    if [ $? -nq 0 ]
+    if [ $? -eq 0 ]
      then
-       dnf install mysql -y
-         if [ $? -eq  0 ]
-         then
-            echo "MYSQL Installed....Successful"
-         # echo "Git Installed.... Sucessful"
-         else 
-        
-          echo "MYSQL Installed....Failure"
-        
-         fi   
-     else 
      echo "mysql already.....installed" 
+     else
+     dnf install mysql -y
     fi 
     #    dnf install git -y
 
