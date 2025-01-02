5d4614a79abdf9955c07773363fef37cf9155948
yamuna <malini.mahathi@gmail.com>
2025-01-02 13:54:19 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -6,8 +6,13 @@ then
 echo "Error: you must have root acess fot this script"
 exit 1
 fi
-
 dnf install mysql -y
+if [ $? -ne 0]
+then
+ echo "mysql installed.....failure"
+ else 
+ echo  "mysql installed.....sucess"
+ fi
 dnf install git -y
 
 
