702a99dc5f81c1e759c8d19532205f38c9906052
yamuna <malini.mahathi@gmail.com>
2025-01-02 13:57:42 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -10,9 +10,16 @@ dnf install mysql -y
 if [ $? -ne 0]
 then
  echo "mysql installed.....failure"
+ exit 1
  else 
  echo  "mysql installed.....sucess"
  fi
 dnf install git -y
 
-
+if [ $? -ne 0]
+then
+ echo "git installed.....failure"
+ exit 1
+ else 
+ echo  "git installed.....sucess"
+ fi
