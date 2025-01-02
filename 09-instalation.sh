624f62ffe0cb83b97254e879999fb7af908ac36d
yamuna <malini.mahathi@gmail.com>
2025-01-02 12:20:30 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -9,8 +9,10 @@ then
     if [ $? -eq 0 ]
      then
         echo "MYSQL Installed....Successful"
+        echo "Git Installed.... Sucessful"
        else 
         echo "MYSQL Installed....Failure"
+        echo "Git Installed.... Failure"
      fi   
   else      
         echo "Error: you must have sudo access to run this script"
