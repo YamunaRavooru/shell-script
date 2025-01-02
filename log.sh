d8ab2c1d5ad940f47a77c2dd06abb64ef8565375
yamuna <malini.mahathi@gmail.com>
2025-01-02 21:47:18 +0530

    script for function execution

---
+++ log.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP"
+validate() {
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2....$R Failure"
+      exit 1
+     else
+      echo  -e "$2..... $G Success" 
+    fi  
+}
+echo "Script started executing at :$TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0 ]
+then
+  echo -e " Erorr:$R Only Root user access this script "
+  exit 1
+fi
+dnf list installed mysql &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    validate $?  "Installing mysql"
+ else
+   echo -e  "mysql is already  $Y installed "   
+fi
+dnf list installed git  &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then   
+   dnf install git -y  &>>$LOG_FILE_NAME
+   validate $? "Installing git"
+else
+   echo  -e "git is already  $Y installed"
+fi      
