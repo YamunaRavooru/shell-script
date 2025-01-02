b50007d544a276428c8d43904380e3eab5796be1
yamuna <malini.mahathi@gmail.com>
2025-01-02 12:18:08 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -5,13 +5,15 @@ if [ $USERID -eq 0 ]
 then
     dnf install mysql -y
     dnf install git -y
-fi
-if [ $? -eq 0 ]
-then
-  echo "MYSQL Installed....Successful"
- else 
-   echo "MYSQL Installed....Failure"
-    echo "Error: you must have sudo access to run this script"
+
+    if [ $? -eq 0 ]
+     then
+        echo "MYSQL Installed....Successful"
+       else 
+        echo "MYSQL Installed....Failure"
+     fi   
+  else      
+        echo "Error: you must have sudo access to run this script"
   fi 
    
     
