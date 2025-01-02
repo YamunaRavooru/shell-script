50f58a3032945d39b5ba5122475d35b8b8a34fca
yamuna <malini.mahathi@gmail.com>
2025-01-02 12:14:46 +0530

    script for inatallation

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -6,8 +6,15 @@ then
     dnf install mysql -y
     dnf install git -y
 fi
-exit 1
-echo "Error: you must have sudo access to run this script"  
-
+if [$? -eq 0 ]
+then
+  echo "MYSQL Installed....Successful"
+ else 
+   echo "MYSQL Installed....Failure"
+    echo "Error: you must have sudo access to run this script"
+  fi 
+   
+    
+  
 
 
