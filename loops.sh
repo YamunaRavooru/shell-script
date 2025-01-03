d1b5a651e5a4cec4f7a2fb5021cf67b078c8ea69
yamuna <malini.mahathi@gmail.com>
2025-01-03 07:00:00 +0530

    scripting for for loop

---
+++ loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP"
+validate() {
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2....$R Failure $N"
+      exit 1
+     else
+      echo  -e "$2..... $G Success $N" 
+    fi  
+}
+echo "Script started executing at :$TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0 ]
+then
+  echo -e " Erorr:$R Only Root user access this script $N "
+  exit 1
+fi
+for packages in $@
+do
+   dnf list install $Packages  &>>LOG_FILE_NAME
+   if [ $? -ne 0 ]
+   then 
+     dnf install $packages -y  &>>LOG_FILE_NAME
+     exit 1
+   else  
+      echo -e "$packages is already   $Y.....installed $N"
+   fi
+done      
