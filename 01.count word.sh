8b220d012c9ed802eafec6a1ea1f05ea25c16c5c
yamuna <malini.mahathi@gmail.com>
2025-01-04 06:33:55 +0530

    adding word

---
+++ 01.count word.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+validate() {
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2....$R Failure $N"
+      exit 1
+     else
+      echo  -e "$2..... $G Success $N" 
+    fi  
+}
+dnf install mysql -y  &>>LOG_FILE_NAME
