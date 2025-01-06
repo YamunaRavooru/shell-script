e257d587cd226b896d50931ad26267411d222100
yamuna <malini.mahathi@gmail.com>
2025-01-06 20:27:31 +0530

    backup files

--- backup.sh
+++ backup.sh
@@ -24,8 +24,30 @@ fi
 if [ ! -d $SOURCE_DIR ]
 then
  echo -e "$SOURCE_DIR does not exist.....please check"
+ exit 1
 fi
-if [ ! -d DESTINATION_DIR ]
+if [ ! -d $DESTINATION_DIR ]
 then
  echo -e "$DESTINATION_DIR does not exist.....please check"
+ exit 1
 fi
+echo "script started execute at :$TIMESTAMP" >>$LOG_FILE_NAME 
+ file=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+ if [ -n "$file" ]
+ then
+    echo "files are :$file"
+    ZIP_FILE="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip  -@ "$ZIP_FILE"
+    if [ -f  "$ZIP_FILE" ]
+    then
+    echo -e "sucessfuly created zip files older than $days"
+     while read -r filename
+     do
+      echo "deleting files:$filename"
+       rm -rf $filename
+       echo "deleted files:$filename"
+     done <<< $file
+    else
+    echo -e "$R ERORR: Failed to create zip file"
+
+    fi
