e7a41ba6c6aae813d98fbfd70593662794ac0b46
yamuna <malini.mahathi@gmail.com>
2025-01-06 20:00:21 +0530

    backup files

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+DAYS=${3:-14}
+LOG_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+USAGE() {
+
+    echo  -e "$R USAGE:$N sh backup.sh <SOURCE_DIR>  <DESTINATION_DIR> <DAYS(optional)>"
+    exit 1
+}
+mkdir -p  /home/ec2-user/shellscript-logs
+if [ $# -lt 2 ]
+then
+   USAGE
+fi
+if [ ! -d $SOURCE_DIR ]
+then
+ echo -e "$SOURCE_DIR does not exist.....please check"
+fi
+if [ ! -d DESTINATION_DIR ]
+then
+ echo -e "$DESTINATION_DIR does not exist.....please check"
+fi
