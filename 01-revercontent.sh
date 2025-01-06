9048c2f72dac8a66a52a703873ff22f892e1bf4c
yamuna <malini.mahathi@gmail.com>
2025-01-06 13:57:55 +0530

    reverse content

--- 01-revercontent.sh
+++ 01-revercontent.sh
@@ -1,14 +1,4 @@
 #!/bin/bash/
 
-LOG_FOLDER="/var/log/expense-logs"
-LOG_FILE=$(echo $0| cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-
-# while read -r file
-# do
-#   echo $file
-
-# done <  01-variable.sh
- file=$(perl -e loops.sh)
- echo $file
+file=$(tac loops.sh)
+echo print the reverse content in file:$file
