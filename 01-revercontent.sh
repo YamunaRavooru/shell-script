75a2dfd9f687f78ebae0a8f436e20384ec8f63b5
yamuna <malini.mahathi@gmail.com>
2025-01-06 13:45:19 +0530

    reverse content

--- 01-revercontent.sh
+++ 01-revercontent.sh
@@ -5,8 +5,8 @@ LOG_FILE=$(echo $0| cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-while read -r line
+while read -r file
 do
-  echo $line
+  echo $file
 
 done < 01-hello.sh
