03eeb3b4818909dcad1cfd4e48a3a2b17756dd99
yamuna <malini.mahathi@gmail.com>
2025-01-07 18:49:03 +0530

    replace word

--- replaceword.sh
+++ replaceword.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-file=$(grep -w hello 02-variable.sh)
-$file=$1
+echo "Find the file you want to search the word in?"
+read filename
+echo "Enter the word you want to find."  
+read word1
+grep $word1 $filename
