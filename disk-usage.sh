b23df3a402c847310fb2d6bed8109d77f65dab98
yamuna <malini.mahathi@gmail.com>
2025-01-07 21:23:21 +0530

    disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
  DISK_USAGE=$(df -hT |grep xfs)
  DISK_THRESHOLD=5 # real projects more than 70
+ MSG=""
  while read -r line
  do
-  USAGE=$(echo $line |awk -F " " '{print $6F}')
+  USAGE=$(echo $line |awk -F " " '{print $6F}'| cut -d "%" -f1)
   PARTITION=$(echo $line |awk -F " " '{print $NF}')
   echo "partition =$PARTITION ,USAGE=$USAGE"
+  if [$USAGE -ge $DISK_THRESHOLD ]
+  then
+    MSG+="High disk usage on partition:$PARTITION usage is:$USAGE \n"
+  fi 
 
  done <<< $DISK_USAGE
+ echo -e "message:$MSG"
