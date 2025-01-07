e85e83235db0eaaacbb38485089f383e89b7df6c
yamuna <malini.mahathi@gmail.com>
2025-01-07 13:15:25 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ SOURCE_DIR=$1
 DESTINATION_DIR=$2
 DAYS=${3:-14}
 LOG_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}'|cut -d "." -f1)
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 USAGE() {
