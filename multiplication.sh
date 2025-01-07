92c8179053d5c7117748d5baa0f7af03a9dcbeed
yamuna <malini.mahathi@gmail.com>
2025-01-07 07:17:26 +0530

    muliplication of number

--- multiplication.sh
+++ multiplication.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+DAYS=${3:-14}
+LOG_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+USAGE() {
 
+    echo  -e "$R USAGE:$N sh backup.sh <SOURCE_DIR>  <DESTINATION_DIR> <DAYS(optional)>"
+    exit 1
+}
+if [ $# -lt 2 ]
+then
+   USAGE
+fi
 echo Enter the number for multiplication: $1
  
 
